6e03900e5fb35f8adcde2cad705e2d073bf98847
crccheck <c@crccheck.com>
2016-12-20 02:51:26 +0000

    Let the csv script take in bbox string

--- csv.py
+++ csv.py
@@ -1,10 +1,13 @@
 """
 Utility to get coordinates from Foursquare checkin history JSON.
 
-Usage: python csv.py topleft bottomright
+Usage:
+    python csv.py <topleft> <bottomright>
+    python csv.py <bboxstring>
 
-Example: python csv.py 36.5,-106.65 25.837,-93.5 | pbcopy
-
+Example:
+    python csv.py 36.5,-106.65 25.837,-93.5 | pbcopy
+    python csv.py -74.0237331390381,40.69580229372344,-73.96682739257814,40.75258879956551
 """
 import json
 import sys
@@ -28,14 +31,20 @@ def get_all_points(top, right, bottom, left):
 
 
 if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        lat1, lng1 = map(float, sys.argv[1].split(','))
+        lat2, lng2 = map(float, sys.argv[2].split(','))
+        top = max(lat1, lat2)
+        right = max(lng1, lng2)
+        bottom = min(lat1, lat2)
+        left = min(lng1, lng2)
+    elif len(sys.argv) == 2:
+        left, bottom, right, top = map(float, sys.argv[1].split(','))
+    else:
+        print(__doc__)
+        exit(1)
+
+    counts = Counter(get_all_points(top, right, bottom, left))
     print('lat,lng,count')
-    lat1, lng1 = map(float, sys.argv[1].split(','))
-    lat2, lng2 = map(float, sys.argv[2].split(','))
-    counts = Counter(get_all_points(
-        max(lat1, lat2),
-        max(lng1, lng2),
-        min(lat1, lat2),
-        min(lng1, lng2),
-    ))
     for (lat, lng), count in counts.items():
         print('{},{},{}'.format(lat, lng, count))
